9c10aef4f15effa8478138a1618dde7cf11fff8d
molink-huseong <huseong.lee@molink.life>
2022-06-24 18:22:39 +0900

    pageVisibility 변경

--- src/Services/AuthorityService.ts
+++ src/Services/AuthorityService.ts
@@ -1,90 +1,25 @@
 import ContentRepo from '../Repositories/ContentRepo'
 import FollowerRepo from '../Repositories/FollowRepo'
 import {
-    DocumentVisibility, ESPageSummary, ESPageSummaryWithVisibility,
+    BlogAuthority,
     GetDocumentAuthorityDTO,
-    HierarchyDocumentInfoInterface, PageVisibility,
-    User, GetBlogAuthorityResponseDTO, BlogAuthority
+    HierarchyDocumentInfoInterface,
+    PageAuthority,
+    PageVisibility,
+    User
 } from '@newturn-develop/types-molink'
 import HierarchyRepo from '../Repositories/HierarchyRepo'
-import { PageNotExist, DocumentUserNotExists, HierarchyNotExists } from '../Errors/DocumentError'
+import { DocumentUserNotExists, PageNotExist } from '../Errors/DocumentError'
 import UserRepo from '../Repositories/UserRepo'
-import { numberToPageVisibility } from '../Utils/NumberToPageVisibility'
 import CacheService from './CacheService'
 import { Slack } from '@newturn-develop/molink-utils'
 import env from '../env'
-import { UserNotExists } from '../Errors/Common'
 import BlogFollowRepo from '../Repositories/BlogFollowRepo'
 import { BlogNotExists } from '../Errors/BlogError'
 import BlogRepo from '../Repositories/BlogRepo'
 import BlogUserRepo from '../Repositories/BlogUserRepo'
 
 class AuthorityService {
-    checkPageViewable (viewer: User, hierarchyDocumentInfo: HierarchyDocumentInfoInterface, isFollower: boolean) {
-        const {
-            visibility,
-            userId: documentUserId
-        } = hierarchyDocumentInfo
-        // 1. 공개된 문서면 무조건 성공
-        if (visibility === PageVisibility.Public) {
-            return true
-        }
-
-        // 2. 로그인 하지 않은 경우, 공개된 문서가 아니므로 무조건 실패
-        if (!viewer) {
-            return false
-        }
-
-        // 3. 주인인 경우 무조건 성공
-        if (documentUserId === viewer.id) {
-            return true
-        }
-
-        // 4. 비공개 문서인 경우, 주인이 아니므로 무조건 실패
-        if (visibility === PageVisibility.Private) {
-            return false
-        }
-
-        // 5. 친구 공개 문서인 경우
-        if (visibility === PageVisibility.OnlyFollower) {
-            return isFollower
-        }
-        throw new Error('Unhandled Document Viewable')
-    }
-
-    checkPageViewableForESSummary (viewer: User, pageOwnerID: number, visibilityNumber: number, isFollower: boolean) {
-        const visibility = numberToPageVisibility(visibilityNumber)
-        // 1. 공개된 문서면 무조건 성공
-        if (visibility === PageVisibility.Public) {
-            return true
-        }
-
-        // 2. 로그인 하지 않은 경우, 공개된 문서가 아니므로 무조건 실패
-        if (!viewer) {
-            return false
-        }
-
-        // 3. 주인인 경우 무조건 성공
-        if (pageOwnerID === viewer.id) {
-            return true
-        }
-
-        // 4. 비공개 문서인 경우, 주인이 아니므로 무조건 실패
-        if (visibility === PageVisibility.Private) {
-            return false
-        }
-
-        // 5. 친구 공개 문서인 경우
-        if (visibility === PageVisibility.OnlyFollower) {
-            return isFollower
-        }
-        throw new Error('Unhandled Page Visibility')
-    }
-
-    checkDocumentEditable (viewer: User, hierarchyDocumentInfo: HierarchyDocumentInfoInterface) {
-        return viewer.id === hierarchyDocumentInfo.userId
-    }
-
     async checkIsFollower (targetId: number, viewerId: number) {
         const followers = await FollowerRepo.getUserFollowers(targetId)
         return followers.map(follower => follower.id).includes(viewerId)
@@ -94,7 +29,7 @@ class AuthorityService {
         return BlogFollowRepo.checkUserFollowBlog(blogID, userID)
     }
 
-    async getPageHierarchyInfo (pageId: string): Promise<HierarchyDocumentInfoInterface> {
+    async getPageInfo (pageId: string): Promise<HierarchyDocumentInfoInterface> {
         try {
             const rawPage = await CacheService.hierarchy.get(`page-${pageId}`)
             if (rawPage) {
@@ -127,20 +62,39 @@ class AuthorityService {
     }
 
     async getPageAuthorityByPageId (viewer: User, pageId: string) {
-        const hierarchyPageInfo = await this.getPageHierarchyInfo(pageId)
-        const pageUserId = hierarchyPageInfo.userId
-        const pageUser = await UserRepo.getActiveUserById(pageUserId)
-        if (!pageUser) {
-            throw new UserNotExists()
-        }
+        const hierarchyPageInfo = await this.getPageInfo(pageId)
+        const blog = hierarchyPageInfo.blogID && await BlogRepo.getBlog(hierarchyPageInfo.blogID)
 
-        const isFollower = viewer && await this.checkIsFollower(pageUserId, viewer.id)
-        const viewable = this.checkPageViewable(viewer, hierarchyPageInfo, isFollower)
-        const editable = this.checkDocumentEditable(viewer, hierarchyPageInfo)
-        if (!viewable) {
-            return new GetDocumentAuthorityDTO(null, null, null, viewable, editable)
+        if (blog) {
+            const blogUser = viewer && await BlogUserRepo.getBlogUser(blog.id, viewer.id)
+            if (blog.is_private) {
+                if (blogUser) {
+                    return new PageAuthority(true, true)
+                } else {
+                    return new PageAuthority(false, false)
+                }
+            } else {
+                if (hierarchyPageInfo.visibility === PageVisibility.Public) {
+                    return new PageAuthority(!!blogUser, true)
+                }
+                if (!viewer) {
+                    return new PageAuthority(false, false)
+                }
+                if (blogUser) {
+                    return new PageAuthority(true, true)
+                }
+                if (hierarchyPageInfo.visibility === PageVisibility.OnlyFollower) {
+                    const isFollower = await BlogFollowRepo.checkUserFollowBlog(blog.id, viewer.id)
+                    if (isFollower) {
+                        return new PageAuthority(!!blogUser, true)
+                    } else {
+                        return new PageAuthority(false, false)
+                    }
+                }
+                // private 인 경우
+                return new PageAuthority(false, false)
+            }
         }
-        return new GetDocumentAuthorityDTO(pageUserId, pageUser.nickname, hierarchyPageInfo.title, viewable, editable)
     }
 
     async getBlogAuthority (viewer: User, blogID: number) {
